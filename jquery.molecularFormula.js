30828e73cd3e1e972c3598c979921b0487a35cbd
radilr <radil@fandrop.com>
2014-08-04 19:53:27 +0300

    fix js error

--- jquery.molecularFormula.js
+++ jquery.molecularFormula.js
@@ -3,6 +3,37 @@
  *  parse (boolean) - default false. If this parameter is provided it converts from H₂O to plain text H2O
  *                    it should be used on form submit to avoid adding sub characters in the database
  */
+$.molecularFormula = function(value, parse) {
+    var chars = '+−=()0123456789aeoxəijruvβγδφχ',
+        sup   = '⁺⁻⁼⁽⁾⁰¹²³⁴⁵⁶⁷⁸⁹ᵃᵉᵒˣᵊⁱʲʳᵘᵛᵝᵞᵟᵠᵡ',  //For future use
+        sub   = '₊₋₌₍₎₀₁₂₃₄₅₆₇₈₉ₐₑₒₓₔᵢⱼᵣᵤᵥᵦᵧᵨᵩᵪ',
+        self  = this;
+
+    function sub_char(char) {
+        var n=chars.indexOf(char)
+        return n == -1 ? char : sub[n]
+    }
+
+    function normal_char(char) {
+        var n=sub.indexOf(char)
+        return n == -1 ? char : chars[n]
+    }
+
+    if (parse) {
+        return value.replace(new RegExp('['+sub+']', "g"), function(x) {
+            return x.split('').map(function(c) { return normal_char(c) }).join('')
+        });
+    } else {
+        return value.replace(/[0-9]*/g, function(x) {
+            return x.split('').map(function(c) { return sub_char(c) }).join('')
+        });
+    }
+}
+
+/*
+ *   Usage:
+ *     $('input.fomula').molecularFormula()
+ */
 $.fn.molecularFormula = function() {
     var elements = [
         'H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl',
